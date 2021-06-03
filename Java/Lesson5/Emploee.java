9b5ba882eaf3e231618c5941b074533f04a79ce7
unknown <subbotnikov@gmail.com>
2021-06-03 17:58:47 -0400

    Lesson5

---
+++ Java/Lesson5/Emploee.java
@@ -0,0 +1,48 @@
+package Lesson5;
+
+public class Emploee {
+
+            private String FirstName;
+            private String LastName;
+            private String MiddleName;
+            private String Positiion;
+            private String Email;
+            private int PhoneNumber;
+            private int Salary;
+            public int Age;
+
+            public Emploee(String FirstName, String LastName, String MiddleName, String Position, String Email,
+                           int PhoneNumber, int Salary, int Age){
+                this.FirstName = FirstName;
+                this.LastName = LastName;
+                this.MiddleName = MiddleName;
+                this.Positiion = Position;
+                this.Email = Email;
+                this.PhoneNumber = PhoneNumber;
+                this.Salary = Salary;
+                this.Age = Age;
+            }
+
+
+
+            public void EmploeeInfo() {
+                System.out.println("FirstName: " + FirstName + "\n" + "LastName: " + LastName + "\n" +
+                        "MiddleName: " + MiddleName + "\n" + "Position: " + Positiion + "\n" +
+                        "Email: " + Email + "\n" +  "PhoneNumber: " + PhoneNumber + "\n" + "Salary: " + Salary + "\n" +
+                        "Age: " + Age );
+
+            }
+            public void AgeInfo(){
+                if (Age > 40) {
+                    EmploeeInfo();
+                }
+            }
+
+
+        }
+
+
+
+
+
+
